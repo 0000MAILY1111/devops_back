720b656689eb7b0da62b9c9b6bfe39fdca1b8e05
facundocg06 <facundocuellarg@gmail.com>
2025-09-25 14:10:52 -0400

    cambios en el projectcontroller

--- src/controllers/ProjectControllers.ts
+++ src/controllers/ProjectControllers.ts
@@ -27,7 +27,12 @@ export class ProjectController {
     static getProjectById = async (req: Request, res: Response) => {    
         const {id} = req.params
         try {
-            const project = (await Project.findById(id)).populated("tasks");
+            //const project = (await Project.findById(id)).populate("tasks");
+            const project = (await Project.findById(id))
+            if (!project) {
+                const error = new Error("proyecto no encontrado");
+                res.status(404).json({error : error.message});
+            }
             res.json(project);
         }catch (error) {
             console.log(error);
@@ -38,14 +43,12 @@ export class ProjectController {
     static updateProject = async (req: Request, res: Response) => {    
         const {id} = req.params
         try {
-            const project = await Project.findById (id);
+            const project = await Project.findByIdAndUpdate(id,req.body);
             if (!project) {
                 const error = new Error("proyecto no encontrado");
                 res.status(404).json({error : error.message});
             }
-            project.projectName = req.body.projectName;
-            project.clientName = req.body.clientName;
-            project.description = req.body.description;
+           
             await project.save();
             res.send("proyecto actualizado correctamente");
         }catch (error) {
